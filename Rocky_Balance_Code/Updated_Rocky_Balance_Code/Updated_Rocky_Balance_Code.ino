6fff2f1d889c5c6981cb9c97e63bb29b1e81a247
Megan Ku <mku@olin.edu>
2020-03-16 21:29:34 -0400

    Updated music

--- Rocky_Balance_Code/Updated_Rocky_Balance_Code/Updated_Rocky_Balance_Code.ino
+++ Rocky_Balance_Code/Updated_Rocky_Balance_Code/Updated_Rocky_Balance_Code.ino
@@ -71,14 +71,15 @@ Balboa32U4ButtonA buttonA;
 #define FIXED_ANGLE_CORRECTION (0.28)  // 0.28 is the value we obtained from the Gyro calibration procedure
 
 const char fugue[] PROGMEM =
-"T98 L4 rr L8 f#ef#ef#e L16 f# L8 ef#a L4 g# L2 r" 
+"T98 L8 MS >f#>f#>f#>f#>f#>f#>f# L16 ML >f#>d L8 MS r>d>d>d>d>d>d L16  ML >d>e L8 MS r>e>e>e>e>e>e L16 ML >e>c# L8 MS r >c#>c#>c#>c#>c#"
+"ML L4 L8 f#ef#ef#e L16 f# L8 ef#a L4 g# L2 r" 
 "L16 a L8 ag#. L16 f# L8 f#. L16 e L8 f#ef#a L4 g# L4 r. L16 r"
 "L8 ef#ef#ef#e L16 f# L8 ef#a L4 g# L4 r. L16 r"
 "L16 f#g# L8 ag#. L16 f# L8 f#. L16 e L8 f#. L16e L8f#a. L4 g#r>c#b "
 "L8 bababa L16 g# L8 g# g#.e L4 f# L8 r. L16 f#e L8 a L4 g# L8 ra.g#. L16 f# L8 f#f#. L4 arb"
 "L8 rag#f#g#f#g#f# L16 f# L8 f# L4 a L2 r L16 f#a L8 ag#. L16 f# L8 g#. L16 f# L8 g# L16 f# L8 f#.a. L4 g# L8 r. MS f# L4 f#f#" 
 "ML L8 a L16 aa L8 g# L16 g#g# L8 g# L16 f#f# L8 a. L4 f# L2 r L16 f#a L8 ag#. L16 g#f#g#f#g# L8 f#g# L16 f# L8 ag#f#a L4 rf#f#"
-"L8 a L16 aa L8 g# L16 g#g# L8 g# L16 f#f# L8 a. L4 f# L2 r L16 f#a L8 ag#. L8 f#g#f# L16 g# L8 f#a.g#f#a";
+"L8 a L16 aa L8 g# L16 g#g# L8 g# L16 f#f# L8 a. L4 f# L2 r L16 f#a L8 ag#. L8 f#g#f# L16 g# L8 f#a.g#f#a L2 r.";
 
 //////////////////////////////////////////////////////////////////////////////////////////////
 // This is the main function that performs the balancing
